63b902dc91b4e3e4e4e5a20253bc8383d9b7f994
Mr.Z <zrd0921@qq.com>
2023-04-13 09:52:40 +0800

    fix(Table): style effect & scroller refresh

--- src/table/table.vue
+++ src/table/table.vue
@@ -492,10 +492,12 @@ export default defineComponent({
 		 * 当 Table 或其祖先元素由隐藏切换为显示时，可能需要调用此方法
 		 */
 		const refreshLayout = () => {
-			layout.updateColumnsWidth();
+			layout.updateColumnsWidth();			
 			if (shouldUpdateHeight.value) {
 				layout.updateElsHeight();
 			}
+
+			scroller.value.refresh();
 		};
 		/**
 		 * 用于多选表格，切换所有行的选中状态
